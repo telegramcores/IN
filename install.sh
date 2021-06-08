446ddc4fbad5cca6764e5e14c8ccad3b3566bd99
telegramcores <69624248+telegramcores@users.noreply.github.com>
2021-06-08 13:30:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 disk="/dev/sda"
 lvm_group_name = "vg01"
-echo "--- очищаем диск ---"
+echo "--- clean disk /dev/sda ---"
 wipefs -af $disk
-echo "--- удаляем LVM группы ---" 
-existing_lvm_groups=vgs | sed -n 2,\$p | awk '{print $1}'
+echo "--- clear LVM group ---" 
+existing_lvm_groups=$(vgs | sed -n 2,\$p | awk '{print $1}')
 vgremove -y $existing_lvm_groups
