cea795ecc8e45486aec3bbcf06ff9b5ab01d5b86
telegramcores <69624248+telegramcores@users.noreply.github.com>
2021-06-08 15:07:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ vgcreate $lvm_group_name /dev/sda3
 lvcreate -y -L 4096M -n swap $lvm_group_name
 lvcreate -y -l 100%VG -n rootfs $lvm_group_name
 
-mkfs.fat -F 32 sda2
+mkfs.fat -F 32 /dev/sda2
 mkfs.ext4 /dev/$lvm_group_name/rootfs
 mkswap /dev/$lvm_group_name/swap
 
