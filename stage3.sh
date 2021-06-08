d5fc7c96ef9fba7b26b97195c02ea6f4cb66f0e9
telegramcores <69624248+telegramcores@users.noreply.github.com>
2021-06-08 12:35:24 +0300

    Update stage3.sh

--- stage3.sh
+++ stage3.sh
@@ -1,43 +1,5 @@
 pushd /mnt/gentoo
 
-    # downloading distfiles list
-
-    MIRROR="https://mirror.yandex.ru"
-    PROCESSOR="amd64"
-    PATH_TO_AUTOBUILDS="gentoo-distfiles/releases/${PROCESSOR}/autobuilds"
-
-    SORTED_OUT=$(mktemp)
-
-    download_list(){
-        URL="$1"
-
-        wget --retry-connrefused \
-             --waitretry=1 \
-             --read-timeout=20 \
-             --timeout=15 \
-             -t 0 \
-            "${URL}" -O - 2>/dev/null | 
-            sed -n -e '/^<a[[:space:]]\+href/p' | 
-            sed 's/<[^>]*>//g' |
-            sort -k 2,2r 
-    }
-
-    download_list "${MIRROR}/${PATH_TO_AUTOBUILDS}" | sed '/\//!d' > "${SORTED_OUT}"
-
-    FOLDER_COOSER='[0-9]\{8\}T[0-9]\{6\}Z'
-    FOLDER=$(cat "${SORTED_OUT}" | grep "${FOLDER_COOSER}" | awk '{print $1}' | sed 1q)
-
-    URL_TO_CHOOSED_STAGE="${MIRROR}/${PATH_TO_AUTOBUILDS}/${FOLDER}"
-    URL_TO_STAGE_FILES="${URL_TO_CHOOSED_STAGE}/$(download_list "${URL_TO_CHOOSED_STAGE}" | 
-                                                    grep "${FOLDER_COOSER}" |
-                                                    grep "stage3-amd64-${FOLDER_COOSER}" |
-                                                    sed -e '/CONTENTS/Id'\
-                                                        -e '/DIGESTS/Id' \
-                                                        -e '/nomultilib/Id' |
-                                                    sed 1q |
-                                                    awk '{print $1}')"
-
-    wget "${URL_TO_STAGE_FILES}"
     URL='http://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds' 
     STAGE3=$(wget $URL/latest-stage3-amd64.txt -qO - | grep -v '#')
     wget $URL/$STAGE3
