7284a66f622ff5933a4057aa430f42627f493596
telegramcores <69624248+telegramcores@users.noreply.github.com>
2021-06-08 13:39:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,11 @@ wipefs -af $disk
 echo "--- clear LVM group ---" 
 existing_lvm_groups=$(vgs | sed -n 2,\$p | awk '{print $1}')
 vgremove -y $existing_lvm_groups
+
+echo "---create sda1 bios_grub ---"
+parted -a optimal --script $disk mklabel gpt
+parted -a optimal --script disk mkpart primary 1MiB 3MiB
+parted -a optimal --script disk name 1 grub
+parted -a optimal --script disk set 1 bios_grub on
+
+
