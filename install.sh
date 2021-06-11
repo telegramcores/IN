cb70e5785c4ef197d4b53de78a1ebbd6c0fc577d
telegramcores <69624248+telegramcores@users.noreply.github.com>
2021-06-11 07:25:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -129,7 +129,7 @@ eselect kernel set 1
 
 << NOT
 echo -e "\e[31m--- create kernel ---\e[0m"
-genkernel --lvm --mountboot --disklabel --busybox --install initramfs all
+genkernel --instal --no-ramdisk-modules --lvm --mountboot --disklabel --busybox l initramfs
 
 grub-install --target=$(lscpu | head -n1 | sed 's/^[^:]*:[[:space:]]*//')-efi --efi-directory=/boot
 grub-mkconfig -o /boot/grub/grub.cfg
