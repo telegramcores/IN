19accbf0f9967ffa09a883ba93de5e412ebcab62
telegramcores <69624248+telegramcores@users.noreply.github.com>
2022-03-02 12:48:03 +0300

    Update recovery.sh

--- recovery.sh
+++ recovery.sh
@@ -7,6 +7,16 @@ swapon /dev/vg01/swap
 mount /dev/vg01/rootfs /mnt/gentoo
 mount /dev/vg02/devhdd /mnt/gentoo/mnt/HDD
 
+# mounting livecd folders
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+mount --bind /run /mnt/gentoo/run
+mount --make-slave /mnt/gentoo/run
+
+
 echo -e "\e[31m--- inside chroot ---\e[0m"
 chroot_dir=/mnt/gentoo
 chroot $chroot_dir /bin/bash << "CHROOT"
