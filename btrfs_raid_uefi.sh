be8875dc78bfbebf01192bc23138818ca9182824
telegramcores <69624248+telegramcores@users.noreply.github.com>
2023-02-12 19:07:27 +0300

    Поддержка ntfs

--- btrfs_raid_uefi.sh
+++ btrfs_raid_uefi.sh
@@ -221,7 +221,7 @@ rc-update add samba default
 ############################
 
 #--- софт ---
-emerge sys-apps/mlocate sys-fs/e2fsprogs app-misc/tmux sys-process/htop app-misc/mc sys-apps/lm-sensors sys-apps/smartmontools app-admin/sudo
+emerge sys-apps/mlocate sys-fs/e2fsprogs app-misc/tmux sys-process/htop app-misc/mc sys-apps/lm-sensors sys-apps/smartmontools app-admin/sudo sys-fs/ntfs3g
 
 echo 'GRUB_PLATFORMS="emu efi-32 efi-64 pc"' >> /etc/portage/make.conf
 echo 'sys-boot/grub:2 device-mapper' >> /etc/portage/package.use/grub2
