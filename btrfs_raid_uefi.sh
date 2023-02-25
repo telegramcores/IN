4f6b96c598734cfa859855cbd1ce12d6e0691db2
telegramcores <69624248+telegramcores@users.noreply.github.com>
2023-02-25 16:02:22 +0300

    Update btrfs_raid_uefi.sh

--- btrfs_raid_uefi.sh
+++ btrfs_raid_uefi.sh
@@ -149,7 +149,7 @@ emerge app-admin/sysklogd && rc-update add sysklogd default
 emerge sys-process/cronie && rc-update add cronie default
 emerge net-misc/dhcpcd && rc-update add dhcpcd default
 emerge net-misc/ntp && rc-update add ntpd default
-rc-update add udev boot
+rc-update add udev sysinit
 emerge sys-fs/btrfs-progs
 rc-update add sshd default
 #Дополнительные настройки для доступа
