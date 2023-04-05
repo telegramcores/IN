3b28a4b94a8e43470069bf98242b62cce4077b46
telegramcores <69624248+telegramcores@users.noreply.github.com>
2023-04-05 09:22:27 +0300

    kernel-bin update

--- btrfs_solo.sh
+++ btrfs_solo.sh
@@ -238,7 +238,7 @@ echo 'GRUB_CMDLINE_LINUX="iommu=pt intel_iommu=on pcie_acs_override=downstream,m
 echo -e "\e[31m--- set kernel ---\e[0m"
 emerge sys-kernel/linux-firmware
 emerge sys-kernel/gentoo-kernel-bin
-dracut -f --kver 6.1.19-gentoo-dist
+dracut -f --kver 6.1.22-gentoo-dist
 
 eselect kernel set 1
 
