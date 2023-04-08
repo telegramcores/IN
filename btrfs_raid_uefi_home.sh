2c8594798f647532331d100b671ead24ae1d70bf
telegramcores <69624248+telegramcores@users.noreply.github.com>
2023-04-08 13:58:12 +0300

    Update btrfs_raid_uefi_home.sh

--- btrfs_raid_uefi_home.sh
+++ btrfs_raid_uefi_home.sh
@@ -87,6 +87,7 @@ STAGE3=$(wget $URL/latest-stage3-amd64-openrc.txt -qO - | grep -v '#' | awk '{pr
 wget $URL/$STAGE3
 echo -e "\e[31m--- extract Stage3 ---\e[0m"
 tar xpf stage3-*.tar.* --xattrs-include='*.*' --numeric-owner
+sed -i '/COMMON_FLAGS=/ s/\("[^"]*\)"/\1 -march=native"/' etc/portage/make.conf
 
 mkdir --parents /mnt/gentoo/etc/portage/repos.conf
 cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
@@ -112,9 +113,6 @@ eselect news read && eselect news purge
 
 # правильный тип процессора в make.conf
 emerge app-misc/resolve-march-native
-march=`resolve-march-native | head -n1 | awk '{print $1;}'`
-sed -i '/COMMON_FLAGS=/ s/\("[^"]*\)"/\1 '$march'"/' /etc/portage/make.conf
-env-update && source /etc/profile
 
 echo '############ бинарные пакеты ##########################'
 cat << EOF >> /etc/portage/binrepos.conf
