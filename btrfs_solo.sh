174d391d86682435a4d5ae3c48078646b7d7c1ca
telegramcores <69624248+telegramcores@users.noreply.github.com>
2023-09-14 18:29:37 +0300

    Update btrfs_solo.sh
    kernel 6.1.53

--- btrfs_solo.sh
+++ btrfs_solo.sh
@@ -75,6 +75,7 @@ emerge-webrsync
 eselect news read && eselect news purge
 
 echo '############ бинарные пакеты ##########################'
+rm /etc/portage/binrepos.conf
 cat << EOF >> /etc/portage/binrepos.conf
 [calculate]
 priority = 9999
@@ -242,7 +243,7 @@ echo 'GRUB_CMDLINE_LINUX="iommu=pt intel_iommu=on pcie_acs_override=downstream,m
 echo -e "\e[31m--- set kernel ---\e[0m"
 emerge sys-kernel/linux-firmware
 emerge sys-kernel/gentoo-kernel-bin
-dracut -f --kver 6.1.46-gentoo-dist
+dracut -f --kver 6.1.53-gentoo-dist
 
 eselect kernel set 1
 
